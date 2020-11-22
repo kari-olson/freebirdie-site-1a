e41b5e8f3653d16d54bf01c70f6ab7568fba7b8e
Glitch (hello-webpage) <none>
2020-11-22 17:38:25 +0000

    🍪👣 Checkpoint ./portfolio-styles.css:21739123/15956 ./index.html:21739123/470 ./css-tricks-smooth-scroll:21739123/982

---
+++ css-tricks-smooth-scroll.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+	$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
+
+
+//CURRENT CLASS
+$(document).ready(function () {
+    $(window).scroll(function () {
+
+        var y = $(this).scrollTop();
+
+        $('.link').each(function (event) {
+            if (y >= $($(this).attr('href')).offset().top - 90) {
+                $('.link').not(this).removeClass('active');
+                $(this).addClass('active');
+            }
+        });
+
+    });
+});
